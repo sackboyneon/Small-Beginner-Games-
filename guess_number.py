329cac6a0866ea4e6452b1547c888ce121666241
Fernando Villegas <fernyvillegas84@gmail.com>
2025-12-10 15:33:21 -0700

    guess_number.py
    en linea 12 olvide poner un int entre player e input para que me pidiera un valor entero

--- guess_number.py
+++ guess_number.py
@@ -8,12 +8,14 @@ player = input("Quieres jugar a adivinar el numero? ")
 seguir_jugando = "si"
 
 while seguir_jugando== "si": 
-    if player == "no":
-        print("Adios!")
-    player = input("Dame un numero: ")
+
+    player = int(input("Dame un numero: "))
     decisiones_compu = random.randint(menor, mayor)
     if decisiones_compu == player:
         print("Felicidades, adivinaste el numero!")
     elif decisiones_compu != player:
         print("Perdiste el numero real era ", decisiones_compu)
+     if player == "no":
+        print("Adios!")    
+
 
